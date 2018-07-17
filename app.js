8d41f85b0f62423d4ac23c7530bb5d0f19e08a4a
RyanSharafuddin <rzs_0000@hotmail.com>
2018-07-17 00:09:29 -0400

    Changed to work for Mafia

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser'); //used for getting body of posts.
 var port = process.env.PORT || 8000;
 
 var bot_id = process.env.BOT_ID;
+var sam_id = "48077875";
 
 app.get('/', function(req, res) {
   console.log("Got request for main page");
@@ -17,21 +18,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded()); //necesary to handle post requests
 
 app.post('/', function(req, res, next) {
-  console.log("Someone posted something.");
-  //console.log(req.body);
-  if(req.body.sender_type != 'bot') {
-    console.log("A non-bot posted something")
+  if(req.body.sender_id == sam_id) {
     request.post(
     'https://api.groupme.com/v3/bots/post',
     { json: {
         "bot_id"  : bot_id,
-        "text"    : ((req.body.sender_id == '57386805') ? "Fardeen has posted" : "Ryan has posted")
+        "text"    : "Disregard whatever " + req.body.name + " just said; it is obvious that he is a mafioso trying to deceive us all."
       }
     }
     );
   }
   else {
-    console.log("A bot posted something");
+
   }
   next();
 });
