d7bd1f62d45dde171c5c48ef8f2f9268b05ba6ab
RyanSharafuddin <rzs_0000@hotmail.com>
2018-07-16 21:30:02 -0400

    Seeing if can get other people's posts.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var http = require('http').Server(app);
+var request = require('request');
 var port = process.env.PORT || 8000;
 
 app.get('/', function(req, res) {
@@ -9,6 +10,24 @@ app.get('/', function(req, res) {
   res.send("Hello World!");
 });
 
+app.post('/', function(req, res, next) {
+  console.log("Someone has posted something.");
+  next();
+});
+
 http.listen(port, function() {
     console.log('listening on *: ' + port);
+    request.post(
+    'https://api.groupme.com/v3/bots/post',
+    { json: {
+        "bot_id"  : "ceba1b427e02f186aa357b9103",
+        "text"    : "Bot restarted."
+      }
+    },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
 });
