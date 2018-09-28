50a9814cccf3e7509b1ab94311c5a7f0db77d046
RyanSharafuddin <rzs_0000@hotmail.com>
2018-09-28 07:42:08 -0400

    First attempt at 20Q

---
+++ aki.js
@@ -0,0 +1,28 @@
+const aki = require('aki-api');
+
+
+// aki.start("en", (resolve, error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(resolve);
+//   }
+// });
+
+// aki.step("en", "7", '1875563862', "0", "9", (resolve, error) => { //ansID, step
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(resolve);
+//   }
+// });
+
+// aki.win("en", "7", '1875563862', "10", (resolve, error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(resolve);
+//   }
+// });
+
+//steps start from 0
--- app.js
+++ app.js
@@ -10,7 +10,15 @@ var path = require('path');
 var http = require('http').Server(app);
 var request = require('request');
 var bodyParser = require('body-parser'); //used for getting body of posts.
+var akinator = require('akinator-api');
 var port = process.env.PORT || 8000;
+
+var twentyQStartValues = ["20Q", "20q", "20 Questions", "20 questions"];
+var inTwentyQ = false; //PUT IN DATABASE
+var step = 0; //DATABASE
+var session; //DATABASE
+var signature; //DATABASE
+var answers = ["0", "1", "2", "3", "4"];
 
 const { Client } = require('pg'); //database stuff
 
@@ -35,6 +43,14 @@ function botPost(text) {
   );
 }
 
+function displayAnswers() {
+  botPost("0 - Yes");
+  botPost("1 - No");
+  botPost("2 - Don't know");
+  botPost("3 - Probably");
+  botPost("4 - Probably not");
+}
+
 app.get('/', function(req, res) {
   console.log("Got request for main page");
   res.send("Hello World!");
@@ -45,42 +61,88 @@ app.use(bodyParser.urlencoded()); //necesary to handle post requests
 
 app.post('/', function(req, res, next) {
 
-if(req.body.name == "Fuddin") {
-  console.log("Has received message from Fuddin; is about to do first client.query()");
-  var fuddinMessages;
-  client.query("SELECT name, num_of_messages FROM messagenums WHERE name = 'Fuddin' ;", (err, res) => {
-        //if (err) throw err;
-        //res.rows is array of rows
-        fuddinMessages = res.rows[0]["num_of_messages"];
-        console.log("After performing query, the value of fuddinMessages is: ");
-        console.log(fuddinMessages);
-        console.log("res.rows is: ")
-        console.log(res.rows);
-        fuddinMessages += 1;
-        client.query("UPDATE messagenums SET num_of_messages = " + fuddinMessages + " WHERE name = 'Fuddin' ;", (err, res) => {
-          botPost("Fuddin has posted " + fuddinMessages + " messages");
-        });
-        //client.end();
-  });
-}
+// if(req.body.name == "Fuddin") {
+//   console.log("Has received message from Fuddin; is about to do first client.query()");
+//   var fuddinMessages;
+//   client.query("SELECT name, num_of_messages FROM messagenums WHERE name = 'Fuddin' ;", (err, res) => {
+//         //if (err) throw err;
+//         //res.rows is array of rows
+//         fuddinMessages = res.rows[0]["num_of_messages"];
+//         console.log("After performing query, the value of fuddinMessages is: ");
+//         console.log(fuddinMessages);
+//         console.log("res.rows is: ")
+//         console.log(res.rows);
+//         fuddinMessages += 1;
+//         client.query("UPDATE messagenums SET num_of_messages = " + fuddinMessages + " WHERE name = 'Fuddin' ;", (err, res) => {
+//           botPost("Fuddin has posted " + fuddinMessages + " messages");
+//         });
+//         //client.end();
+//   });
+// }
 
   if(req.body.name != "Mafia Detector") { //ignore own posts
-    botPost("created_at: " + req.body.created_at
-              + "\nid: " + req.body.id
-              + "\nname: " + req.body.name
-              + "\nsender_id: " + req.body.sender_id
-              + "\nsender_type: " + req.body.sender_type
-              + "\nsource_guid: " + req.body.source_guid
-              + "\nsystem: " + req.body.system
-              + "\nuser_id: " + req.body.user_id);
-
+    // botPost("created_at: " + req.body.created_at
+    //           + "\nid: " + req.body.id
+    //           + "\nname: " + req.body.name
+    //           + "\nsender_id: " + req.body.sender_id
+    //           + "\nsender_type: " + req.body.sender_type
+    //           + "\nsource_guid: " + req.body.source_guid
+    //           + "\nsystem: " + req.body.system
+    //           + "\nuser_id: " + req.body.user_id);
   }
-  if(req.body.sender_id == sam_id) {
+
+  if((req.body.sender_id == sam_id) && (Math.random() < .2)) { //20% chance of accusing Sam.
     botPost("Disregard whatever " + req.body.name + " just said; it is obvious that he is a mafioso trying to deceive us all.");
   }
-  else { //not Sam
+
+  if(twentyQStartValues.includes(req.body.text)) { //start
+    inTwentyQ = true; //DATABASE
+    aki.start("en", (resolve, error) => {
+      if (error) {
+        botPost("20 Questions is broken. Sorry.");
+        inTwentyQ = false; //DATABASE
+      }
+      else {
+        signature = resolve.signature; //DATABASE
+        session = resolve.session; //DATABASE
+        step = 0; //DATABASE
+        botPost(resolve.question);
+        displayAnswers();
+      }
+    });
 
   }
+
+  if(inTwentyQ && answers.includes(req.body.text)) { //DATABASE
+    aki.step("en", session, signature, req.body.text, step, (resolve, error) => { //DATABASE
+      if (error) {
+        botPost("20 Questions is broken. Sorry.");
+        inTwentyQ = false; //DATABASE
+      }
+      else {
+        step += 1; //DATABASE
+        if(resolve.progress >= 85) {
+          //WIN CODE
+          aki.win("en", session, signature, step, (resolve, error) => {
+            if (error) {
+              botPost("20 Questions is broken. Sorry.");
+              inTwentyQ = false; //DATABASE
+            }
+            else {
+              botPost("I guess it is: " + resolve.answers[0].name);
+              inTwentyQ = false; //DATABASE
+              step = 0; //DATABASE
+            }
+          });
+        }
+        else {
+          botPost(resolve.nextQuestion);
+          displayAnswers();
+        }
+      }
+    });
+  }
+
   next();
 });
 
@@ -90,6 +152,7 @@ http.listen(port, function() {
        " you need to set the environment variable using export BOT_ID for it to work." +
        " The export keyword is important.");
     botPost("THE BOT IS NOW ONLINE!");
+
 //     request.post(
 //     'https://api.groupme.com/v3/bots/post',
 //     { json: {
